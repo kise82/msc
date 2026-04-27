976c0ffa8c774fd187c328dcd68dcfd85bbe3c0c
kise82 <kise82@pm.me>
2026-04-27 21:45:34 +0000

    `lexer`: Initial setup
    Signed-off-by: kise82 <kise82@pm.me>

---
+++ src/lexer.rs
@@ -0,0 +1,91 @@
+#[repr(C)]
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Unknown,
+
+    // Operators
+    Plus,
+    Minus,
+    Star,
+    Slash,
+}
+
+#[repr(C)]
+pub struct Lexer<'a> {
+    input: &'a str,
+    remainder: &'a str,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            input,
+            remainder: input,
+        }
+    }
+
+    fn advance(&mut self, chars: usize) {
+        self.remainder = &self.remainder[chars..];
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use Token::*;
+
+        let mut scan = || {
+            let start = if let Some((index, _)) = self
+                .remainder
+                .chars()
+                .enumerate()
+                .find(|(_, c)| !c.is_whitespace())
+            {
+                index
+            } else {
+                self.remainder.len()
+            };
+            self.remainder = &self.remainder[start..];
+        };
+
+        scan();
+
+        let mut chars = self.remainder.chars();
+        let token = match chars.next()? {
+            '+' => Plus,
+            '-' => Minus,
+            '*' => Star,
+            '/' => Slash,
+            _ => Unknown,
+        };
+
+        self.advance(1);
+
+        Some(token)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn operators() {
+        let mut lexer = Lexer::new("+  -   *  / ");
+
+        assert_eq!(lexer.next(), Some(Token::Plus));
+        assert_eq!(lexer.next(), Some(Token::Minus));
+        assert_eq!(lexer.next(), Some(Token::Star));
+        assert_eq!(lexer.next(), Some(Token::Slash));
+    }
+
+    #[test]
+    fn blanks() {
+        let mut lexer = Lexer::new("");
+        assert_eq!(lexer.next(), None);
+
+        let mut lexer = Lexer::new("   ");
+        assert_eq!(lexer.next(), None);
+    }
+}
